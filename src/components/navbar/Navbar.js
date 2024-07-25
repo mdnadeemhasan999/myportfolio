89c8b4d555bbe7f13d8d3ef468239982a9637227
mdnadeemhasan999 <mdndeemhasan999@gmail.com>
2024-07-25 13:14:20 +0530

    change some codet

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaCloudDownloadAlt, FaLinkedinIn } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 import { logo } from "../../assets/index";
 import { navLinksdata } from "../../constants";
 
