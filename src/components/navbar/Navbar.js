256324a7d2d51dfec125b5b8faa0b60ea1be5373
mdnadeemhasan999 <mdndeemhasan999@gmail.com>
2024-07-25 12:50:23 +0530

    logo commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import {logo} from "../../assets/index"
-import { navLinksdata } from '../../constants';
+import { logo } from "../../assets/index";
+import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
@@ -45,9 +45,9 @@ const Navbar = () => {
               <div>
                 <img className="w-32" src={logo} alt="logo" />
                 <p className="text-sm text-gray-400 mt-2">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Earum soluta perspiciatis molestias enim cum repellat, magnam
-                  exercitationem distinctio aliquid nam.
+                  Experienced web developer skilled in React.js and WordPress.
+                  Delivers high-performance websites, collaborates effectively,
+                  exceeds client expectations, drives growth.
                 </p>
               </div>
               <ul className="flex flex-col gap-4">
@@ -75,15 +75,33 @@ const Navbar = () => {
                   Find me in
                 </h2>
                 <div className="flex gap-4">
-                  <span className="bannerIcon">
-                    <FaFacebookF />
-                  </span>
-                  <span className="bannerIcon">
-                    <FaTwitter />
-                  </span>
-                  <span className="bannerIcon">
-                    <FaLinkedinIn />
-                  </span>
+                  <a
+                    href="https://www.linkedin.com/in/mdnadeemhasan/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="bannerIcon">
+                      <FaLinkedinIn />
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/mdnadeemhasan999"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="bannerIcon">
+                      <BsGithub />
+                    </span>
+                  </a>
+                  <a
+                    href="https://drive.google.com/uc?export=download&id=1nqxOMr0HLGEs1Q4px6b-uyUuUsxm1SeT"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="bannerIcon">
+                      <FaCloudDownloadAlt />
+                    </span>
+                  </a>
                 </div>
               </div>
               <span
@@ -98,6 +116,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
